5a6738dbba48c506e575ffb0058e72a28831ef77
Paul <44018884+Squidez@users.noreply.github.com>
2025-05-16 10:02:39 +0200

    Corrections and added comments

--- src/main.js
+++ src/main.js
@@ -38,28 +38,33 @@ function shuffle(array) {
   return copy;
 }
 
-function create_image_set(images_data) {
+// Creates the set of images used in the game
+function create_image_set(images_data, n) {
 
   const categories = ['A', 'B', 'C', 'D','E', 'F', 'G'];
   let image_set = [];
 
   for (const i in categories) {
 
+    // Retrieves all the images of a category
     let cat_images = images_data.filter(function(item){
       return item.category == categories[i];         
     });
 
+    // Retrieves all the images of "type a" 
     let cat_a = cat_images.filter(function(item){
       return item.tag == 'a';
     })
-
+    // Retrieves all the images of "type a" 
     let cat_b = cat_images.filter(function(item){
       return item.tag == 'b';
     })
 
-    cat_a = shuffle(cat_a).slice(0,5);
-    cat_b = shuffle(cat_b).slice(0,5);
+    // Shuffles the images list and keeps five of them
+    cat_a = shuffle(cat_a).slice(0,n);
+    cat_b = shuffle(cat_b).slice(0,n);
 
+    // Shuffles the two images types and adds them to the set
     shuffle(cat_a.concat(cat_b)).forEach(img => {
       image_set.push(img)
     });
@@ -68,9 +73,9 @@ function create_image_set(images_data) {
   return image_set;
 }
 
+// Declare variables
 let score = 0;
-const image_set = create_image_set(images_data)
-
+const image_set = create_image_set(images_data, 5)
 const labels = {
     'A': ['Chihuahua', 'Muffin'],
     'B': ['Banane', 'Canard'],
@@ -80,29 +85,42 @@ const labels = {
     'F': ['Noix de Coco', 'Paresseux'],
     'G': ['Chat', 'Pain Pita']}
 
-// Show the first image
+// Display the first image and button labels
 let current_image = image_set[0];
 image_element.src = current_image.path;
 let last_category = current_image.category
+button_a.innerHTML = labels[last_category][0];
+button_b.innerHTML = labels[last_category][1];
 let image_index = 1;
 
 function check_image(key) {
-  // if (key === 'a' || key === 'b' ) {
+  
+  // if the answer is correct
   if (key === current_image.tag) {
+
+    // increment score and it's label
     score++;
     score_element.textContent = score;
   }
   
+  // Display the image according to index
   current_image = image_set[image_index];
   console.log(current_image);
   image_element.src = current_image.path;
+
   //Labels the buttons
   let category = current_image.category;
   button_a.innerHTML = labels[category][0];
   button_b.innerHTML = labels[category][1];
+
+  // If the category changes
   if (category != last_category) {
+
+    // Labels the buttons
     cat_a.innerHTML = labels[category][0];
     cat_b.innerHTML = labels[category][1];
+
+    // Open and animate the new category modal
     open_modal(category_dial);
     animate( category_dial, {
       scale: [0, 1.5],
@@ -112,12 +130,14 @@ function check_image(key) {
     });
     setTimeout(() => {close_modal(category_dial) }, 1000);
   }
+
+  // Updates the category
   last_category = category
+
   //increment image index
   image_index++;
-    
-  // }
 
+  // Displays the Game Over modal when all images have been checked
   if (image_index === image_set.length) {
     gameover_msg.innerHTML = `Bravo, tu as reconnu ${score} images`;
     setTimeout(() => {open_modal(gameover_dial)}, 200);  
@@ -138,17 +158,45 @@ function check_image(key) {
 //   button_b.innerHTML = labels[category][1];
 // }
 
+// Buttons events
+// A type button (Left)
 button_a.addEventListener('click', (event) => {
   check_image('a');
 });
+// B type button (right)
 button_b.addEventListener('click', (event) => {
   check_image('b');
 });
+// Start button
+start_btn.addEventListener('click', (event) => {
+  animate( start_btn, {
+        scale: [0.7, 2],
+        opacity: [1, 0.5],
+        duration: 500,
+        easing: 'easeOutCubic'
+      });
+    setTimeout(() => {close_modal(start_dial)}, 300); 
+});
+// Restart Button
+gameover_btn.addEventListener('click', (event) => {
+  animate( gameover_btn, {
+        scale: [0.7, 2],
+        opacity: [1, 0],
+        duration: 500,
+        easing: 'easeOutCubic'
+      });
+  setTimeout(() => {location.reload()}, 300);
+  // reset_game();
+  // open_modal(start_dial);
+  // close_modal(gameover_dial);
+});
 
+// Keyboard events
 document.addEventListener('keypress', (event) => {
   const key = event.key.toLowerCase();
   if (start_dial.classList.contains('hidden')) {
 
+    // Trigger A button (Left)
     if (key === 'a') {
       
       button_a.classList.add('animate-ping')
@@ -157,6 +205,7 @@ document.addEventListener('keypress', (event) => {
 
       setTimeout(() => {check_image(key);}, 100); 
 
+    // Trigger B button (Left)
     } else if (key === 'b'){
       button_b.classList.add('animate-ping')
       setTimeout(() => {button_b.classList.remove('animate-ping')
@@ -164,7 +213,6 @@ document.addEventListener('keypress', (event) => {
 
       setTimeout(() => {check_image(key);}, 100); 
     }
-   
   }
 
   // Trigger Start button on key press
@@ -190,32 +238,11 @@ document.addEventListener('keypress', (event) => {
   }
 });
 
-start_btn.addEventListener('click', (event) => {
-  animate( start_btn, {
-        scale: [0.7, 2],
-        opacity: [1, 0.5],
-        duration: 500,
-        easing: 'easeOutCubic'
-      });
-    setTimeout(() => {close_modal(start_dial)}, 300); 
-});
-
-gameover_btn.addEventListener('click', (event) => {
-  animate( gameover_btn, {
-        scale: [0.7, 2],
-        opacity: [1, 0],
-        duration: 500,
-        easing: 'easeOutCubic'
-      });
-  setTimeout(() => {location.reload()}, 300);
-  // reset_game();
-  // open_modal(start_dial);
-  // close_modal(gameover_dial);
-});
-
+// Call to open a modal
 function open_modal(modal_elem) {
       modal_elem.classList.remove('hidden');
 }
+// Call to close a modal
 function close_modal(modal_elem) {
       modal_elem.classList.add('hidden');
 }
